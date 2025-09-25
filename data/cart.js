0ae6e9725a8288c1b30f368f34d6c6a8c2e94a19
Babsmay47 <babsmay47@gmail.com>
2025-09-25 17:11:58 +0100

    Save cart into local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 },{
@@ -6,6 +6,9 @@ export let cart = [{
   quantity: 1
 }];
 
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let macthingItem;
@@ -27,6 +30,8 @@ export function addToCart(productId) {
       quantity
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -38,4 +43,5 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart; 
+  saveToStorage();
 }
